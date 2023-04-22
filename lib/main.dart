9f75004e9c71b7bed6a26b00b8321a8ad174e7ba
Nadeen Radwan <nadeenradwan0@gmail.com>
2023-04-22 11:08:41 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:zoom_clone/screens/login_screen.dart';
 import 'package:zoom_clone/utils/color.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      // options: DefaultFirebaseOptions.currentPlatform,
+      );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -19,7 +24,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: backgroundColor,
       ),
       routes: {
-        '/login' : (context) => const LoginScreen(),
+        '/login': (context) => const LoginScreen(),
       },
       home: const LoginScreen(),
     );
