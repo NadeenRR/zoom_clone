97bc206ffe8d3bdffcd8661e81dbb6f11fa082bc
Nadeen Radwan <nadeenradwan0@gmail.com>
2023-04-22 10:41:51 +0300

    onboarding done!

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/custome_button.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        const Text(
+          'Start or join a meeting',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 40),
+          child: Image.asset('assets/images/onboarding.jpg'),
+        ),
+        CustomeButton(
+          text: 'Google Sign In',
+          onPressed: () {},
+        )
+      ]),
+    );
+  }
+}
